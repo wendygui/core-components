d3c617e0546ab7fcf6d5f449a0fabd52aebcae3f
wendygui <98666175+wendygui@users.noreply.github.com>
2022-04-21 20:06:45 -0400

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -9,8 +9,8 @@ import commonjs from '@rollup/plugin-commonjs';
 var componentPath
 var serverPath
 if ((process.env.BUILD !== 'production')) {
-    componentPath = "https://blair-vue-apps.ngrok.io/vue-apps/";
-    serverPath = "https://blair-core.ngrok.io/build/";
+    componentPath = "https://wendygui.github.io/vue-apps/";
+    serverPath = "https://wendygui.github.io/core-components/";
 } else {
     componentPath = "https://resources.realitymedia.digital/vue-apps/";
     serverPath = "https://resources.realitymedia.digital/core-components/";
